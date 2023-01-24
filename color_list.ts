e905770e1da39dd570741280c2530f6ae5a17580
Sayandip Halder <107913804+sayan-dip@users.noreply.github.com>
2023-01-24 10:00:33 +0530

    Create color_list.ts

---
+++ color_list.ts
@@ -0,0 +1,65 @@
+export const COLOR_LIST = [
+  { color: '#1600E5', key: 'a' },
+  { color: '#7566FF', key: 'b' },
+  { color: '#FFD766', key: 'c' },
+  { color: '#FFD766', key: 'd' },
+  { color: '#8C4AE8', key: 'e' },
+  { color: '#8C4AE8', key: 'f' },
+  { color: '#FF9D80', key: 'g' },
+  { color: '#FF9D80', key: 'h' },
+  { color: '#4CE6FF', key: 'i' },
+  { color: '#4CE6FF', key: 'j' },
+  { color: '#FF4C9B', key: 'k' },
+  { color: '#FF4C9B', key: 'l' },
+  { color: '#4DE593', key: 'm' },
+  { color: '#4DE593', key: 'n' },
+  { color: '#C5CCD3', key: 'o' },
+  { color: '#C5CCD3', key: 'p' },
+  { color: '#1600E5', key: 'q' },
+  { color: '#FFCA33', key: 'r' },
+  { color: '#6F1DE2', key: 's' },
+  { color: '#FF6333', key: 't' },
+  { color: '#00DBFF', key: 'u' },
+  { color: '#FF0070', key: 'v' },
+  { color: '#FF0070', key: 'w' },
+  { color: '#9DA6AF', key: 'x' },
+  { color: '#20DF78', key: 'y' },
+  { color: '#20DF78', key: 'z' }
+]
+
+export const EDITOR_LIST = [
+  {
+    avatar:
+      'https://pct-avatars-dev.s3.ap-south-1.amazonaws.com/11ed07f146cfa700ba896b0d4969e38c.jpg',
+    name: 'X One'
+  },
+  {
+    avatar: '',
+    name: 'Editor Two'
+  },
+  {
+    avatar:
+      'https://pct-avatars-dev.s3.ap-south-1.amazonaws.com/11ed07f146cfa700ba896b0d4969e38c.jpg',
+    name: 'Person Three'
+  },
+  {
+    avatar: '',
+    name: 'Human Four'
+  },
+  {
+    avatar: '',
+    name: 'Editor Five'
+  },
+  {
+    avatar:
+      'https://pct-avatars-dev.s3.ap-south-1.amazonaws.com/11ed07f146cfa700ba896b0d4969e38c.jpg',
+    name: 'Editor Six'
+  },
+  {
+    avatar:
+      'https://pct-avatars-dev.s3.ap-south-1.amazonaws.com/11ed07f146cfa700ba896b0d4969e38c.jpg',
+    name: 'Editor Seven'
+  }
+]
+
+// EDITOR_LIST to be deleted
